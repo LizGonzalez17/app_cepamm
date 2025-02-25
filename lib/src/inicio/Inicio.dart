cfdcf26b89fc944ec2c54eb12ec482ba30263614
jairteodocio <jairerickiker@gmail.com>
2025-02-24 21:45:01 -0600

    mostra nombre

--- lib/src/inicio/Inicio.dart
+++ lib/src/inicio/Inicio.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:app_cepamm/src/login/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -31,16 +33,19 @@ class _InicioState extends State<Inicio> {
         viewportFraction: 0.6); // Aumenta el tamaño de las imágenes
 
     _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
-      if (_currentPage < _imagenes.length - 1) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
+      if (_pageController.hasClients) {
+        // Verifica si el controlador está adjunto a una vista
+        if (_currentPage < _imagenes.length - 1) {
+          _currentPage++;
+        } else {
+          _currentPage = 0;
+        }
+        _pageController.animateToPage(
+          _currentPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
       }
-      _pageController.animateToPage(
-        _currentPage,
-        duration: Duration(milliseconds: 500),
-        curve: Curves.easeInOut,
-      );
     });
   }
 
@@ -53,80 +58,110 @@ class _InicioState extends State<Inicio> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Icon(Icons.menu, size: 30),
-                  SizedBox(width: 10),
-                  Text(
-                    '',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ],
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      return Login();
+    }
+
+    return FutureBuilder<QuerySnapshot>(
+        future: FirebaseFirestore.instance
+            .collection('paciente')
+            .where('uid', isEqualTo: user.uid)
+            .limit(1)
+            .get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          if (snapshot.hasError || snapshot.data!.docs.isEmpty) {
+            return Scaffold(
+              body:
+                  Center(child: Text('Error al cargar los datos del usuario')),
+            );
+          }
+          final userData =
+              snapshot.data!.docs.first.data() as Map<String, dynamic>;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                '${userData['nombres']} ${userData['aPaterno']}  ${userData['aMaterno']}',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 8),
-              Text(
-                "fundación CEPAMM",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[400],
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Este es tu camino hacia una nueva sonrisa:",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Image.asset(
-                    'assets/app1.png',
-                    width: 300,
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Promociones:",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              SizedBox(
-                height: 150, // Aumenta la altura del carrusel
-                child: PageView.builder(
-                  controller: _pageController,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _imagenes.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8),
-                      child: ClipRRect(
-                        borderRadius:
-                            BorderRadius.circular(15), // Bordes redondeados
-                        child: Image.asset(
-                          _imagenes[index],
-                          width: 150, // Aumenta el ancho de la imagen
-                          height: 150, // Aumenta la altura de la imagen
-                          fit: BoxFit.cover,
-                        ),
+            ),
+            drawer: const Drawer(),
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "fundación CEPAMM",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[400],
                       ),
-                    );
-                  },
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Este es tu camino hacia una nueva sonrisa:",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Image.asset(
+                          'assets/app1.png',
+                          width: 300,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Promociones:",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      height: 150, // Aumenta la altura del carrusel
+                      child: PageView.builder(
+                        controller: _pageController,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _imagenes.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(
+                                  15), // Bordes redondeados
+                              child: Image.asset(
+                                _imagenes[index],
+                                width: 150, // Aumenta el ancho de la imagen
+                                height: 150, // Aumenta la altura de la imagen
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
